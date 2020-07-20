be1c8c92a17dcc02ce46901fbb08f31331825ceb
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-20 11:29:15 +0800

    Update Business2Client.java

--- business/src/main/java/com/ncs/springcloud/Business2Client.java
+++ business/src/main/java/com/ncs/springcloud/Business2Client.java
@@ -1,7 +1,14 @@
-@FeignClient("business2")
+@FeignClient("business2", fallback = Business2ClientFallback.class)
 public interface Business2Client {
 
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String getTest();
     
+}
+
+static class Business2ClientFallback implements Business2Client {
+    @Override
+    public String getTest() {
+        return "business2-fallback";
+    }
 }
