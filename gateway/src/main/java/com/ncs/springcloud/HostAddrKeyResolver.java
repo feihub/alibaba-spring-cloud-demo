a4236af0dcb670aad990e741415866be0dff105d
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-22 14:06:53 +0800

    Create HostAddrKeyResolver.java

---
+++ gateway/src/main/java/com/ncs/springcloud/HostAddrKeyResolver.java
@@ -0,0 +1,10 @@
+
+public class HostAddrKeyResolver implements KeyResolver {
+
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        return Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+    }
+
+}
+
