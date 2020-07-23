d83a26446221b08abfc2ea841b116d7edcc34f6f
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-23 14:47:01 +0800

    Create UserController.java

---
+++ oauth2/src/main/java/com/ncs/springcloud/UserController.java
@@ -0,0 +1,8 @@
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/getCurrentUser")
+    public Object getCurrentUser(Authentication authentication) {
+        return authentication.getPrincipal();
+    }
+}
