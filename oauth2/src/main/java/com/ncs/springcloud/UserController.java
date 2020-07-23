dfea930c0f8c80804c6ee69b92dfb0e41db7956b
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-23 15:11:26 +0800

    Update UserController.java

--- oauth2/src/main/java/com/ncs/springcloud/UserController.java
+++ oauth2/src/main/java/com/ncs/springcloud/UserController.java
@@ -1,7 +1,7 @@
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @GetMapping("/getCurrentUser")
+    @GetMapping("/info")
     public Object getCurrentUser(Authentication authentication) {
         return authentication.getPrincipal();
     }
