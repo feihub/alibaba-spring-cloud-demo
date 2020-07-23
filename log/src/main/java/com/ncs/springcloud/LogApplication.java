3d60fd33edbb23f53cf388b164caf822f9017e32
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-23 10:30:17 +0800

    Update LogApplication.java

--- log/src/main/java/com/ncs/springcloud/LogApplication.java
+++ log/src/main/java/com/ncs/springcloud/LogApplication.java
@@ -1,36 +1,15 @@
 package com.ncs.springcloud;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableCircuitBreaker
-@RestController
 @EnableZipkinServer
 public class LogApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LogApplication.class, args);
     }
-
-    @RequestMapping("/test")
-    @HystrixCommand(fallbackMethod="logFallback")
-    public String home() {
-
-        int i = 1/0;
-
-        return "log";
-    }
-
-    public String logFallback() {
-        return "logFallback";
-    }
-
 }
