4f6e9a6b9d7c21c5acac27e2a22cc608044f9891
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-23 10:38:04 +0800

    Update ConfigServerApplication.java

--- config/src/main/java/com/ncs/springcloud/ConfigServerApplication.java
+++ config/src/main/java/com/ncs/springcloud/ConfigServerApplication.java
@@ -1,36 +1,15 @@
 package com.ncs.springcloud;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
-@EnableCircuitBreaker
-@RestController
 public class ConfigServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
-
-    @RequestMapping("/")
-    @HystrixCommand(fallbackMethod="configFallback")
-    public String home() {
-
-        int i = 1/0;
-
-        return "config";
-    }
-
-    public String configFallback() {
-        return "configFallback";
-    }
-
 }
