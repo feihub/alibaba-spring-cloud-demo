0cd63b6c69b722d7b52b366813b5dae25322e9ee
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-23 15:15:40 +0800

    Update BusinessApplication.java

--- business/src/main/java/com/ncs/springcloud/BusinessApplication.java
+++ business/src/main/java/com/ncs/springcloud/BusinessApplication.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableFeignClients
 @RefreshScope
+@EnableOAuth2Sso
 public class BusinessApplication {
 
     @Autowired
