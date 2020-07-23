f4fe309e1d1bed5ebae72664ec9f17c996907ecb
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-23 11:13:28 +0800

    Update EurekaServerApplication.java

--- eureka/src/main/java/com/ncs/springcloud/EurekaServerApplication.java
+++ eureka/src/main/java/com/ncs/springcloud/EurekaServerApplication.java
@@ -2,16 +2,12 @@ package com.ncs.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableCircuitBreaker
 public class EurekaServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
-
 }
