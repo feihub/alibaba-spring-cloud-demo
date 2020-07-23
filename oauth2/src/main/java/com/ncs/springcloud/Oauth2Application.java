c3bfcc4eac243ef1bf829f890c62df8197449d18
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-23 14:50:53 +0800

    Create Oauth2Application.java

---
+++ oauth2/src/main/java/com/ncs/springcloud/Oauth2Application.java
@@ -0,0 +1,14 @@
+package com.ncs.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Oauth2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2Application.class, args);
+    }
+}
