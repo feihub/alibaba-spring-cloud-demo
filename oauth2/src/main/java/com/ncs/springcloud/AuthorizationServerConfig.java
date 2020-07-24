72df0b63ae21466ec2261aaacf76f1cf7cea50f0
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-24 09:41:30 +0800

    Update AuthorizationServerConfig.java

--- oauth2/src/main/java/com/ncs/springcloud/AuthorizationServerConfig.java
+++ oauth2/src/main/java/com/ncs/springcloud/AuthorizationServerConfig.java
@@ -35,7 +35,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .secret(passwordEncoder.encode("password"))
                 .accessTokenValiditySeconds(3600)
                 .refreshTokenValiditySeconds(864000)
-                .redirectUris("http://www.baidu.com")
+                .redirectUris("http://localhost:8088/login")
                 .scopes("all")
                 .authorizedGrantTypes("authorization_code","password");
     }
