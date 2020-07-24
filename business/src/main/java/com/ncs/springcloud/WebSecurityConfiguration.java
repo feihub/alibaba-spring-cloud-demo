092f570d6d25902ea17e56365ccb4f77029ed1dd
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-24 15:12:13 +0800

    Update WebSecurityConfiguration.java

--- business/src/main/java/com/ncs/springcloud/WebSecurityConfiguration.java
+++ business/src/main/java/com/ncs/springcloud/WebSecurityConfiguration.java
@@ -1,13 +1,19 @@
+package com.ncs.springcloud;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 @Configuration
 @EnableOAuth2Sso
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
             .authorizeRequests()
-                .antMatchers("/error").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable();
+            .antMatchers("/login", "/oauth/**", "/logout","/error")
+            .permitAll()
+            .anyRequest().authenticated();
     }
 }
