3ae1b79c781518d5d18e619611efc19d8027f490
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-24 09:13:04 +0800

    Update BusinessApplication.java

--- business/src/main/java/com/ncs/springcloud/BusinessApplication.java
+++ business/src/main/java/com/ncs/springcloud/BusinessApplication.java
@@ -4,7 +4,6 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableFeignClients
 @RefreshScope
-@EnableOAuth2Sso
 public class BusinessApplication {
 
     @Autowired
