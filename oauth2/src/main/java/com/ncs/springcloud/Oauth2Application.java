d722196ddc1a5de8ea0bd33332747a0ab95fae3f
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-24 15:17:43 +0800

    Update Oauth2Application.java

--- oauth2/src/main/java/com/ncs/springcloud/Oauth2Application.java
+++ oauth2/src/main/java/com/ncs/springcloud/Oauth2Application.java
@@ -6,8 +6,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class Oauth2Application {
     public static void main(String[] args) {
         SpringApplication.run(Oauth2Application.class, args);
+    }
+    @GetMapping("/")
+    public String home() {
+        return "Oauth2ApplicationOk";
     }
 }
