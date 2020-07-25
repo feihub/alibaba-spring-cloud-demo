2b3f6ede69b383bd6bff4c095e12cc74bef1efd8
xiaofeiwang <xiaofeiwang2016@163.com>
2020-07-25 08:50:45 +0800

    repair bug

--- oauth2/src/main/java/com/ncs/springcloud/Oauth2Application.java
+++ oauth2/src/main/java/com/ncs/springcloud/Oauth2Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
 @EnableEurekaClient
