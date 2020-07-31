695a3ffd8d708fa08795a635338043fddbd9f340
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-31 09:42:25 +0800

    Update ResourceServerConfigurer.java

--- zuul/src/main/java/com/ncs/springcloud/ResourceServerConfigurer.java
+++ zuul/src/main/java/com/ncs/springcloud/ResourceServerConfigurer.java
@@ -5,18 +5,18 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@EnableWebFluxSecurity
+//@EnableWebFluxSecurity
 public class ResourceServerConfigurer {
 
-    private static final String[] AUTH_WHITELIST = new String[]{"/login", "/oauth/**", "/logout","/error","/actuator/**"};
+//     private static final String[] AUTH_WHITELIST = new String[]{"/login", "/oauth/**", "/logout","/error","/actuator/**"};
 
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.csrf().disable()
-                .authorizeExchange()
-                .pathMatchers(AUTH_WHITELIST).permitAll()
-                .anyExchange().authenticated()
-                .and().oauth2ResourceServer().jwt();
-        return http.build();
-    }
+//     @Bean
+//     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+//         http.csrf().disable()
+//                 .authorizeExchange()
+//                 .pathMatchers(AUTH_WHITELIST).permitAll()
+//                 .anyExchange().authenticated()
+//                 .and().oauth2ResourceServer().jwt();
+//         return http.build();
+//     }
 }
