6df2520013e391dea1ffdd02e7165359da704be8
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-31 10:44:31 +0800

    Update Oauth2Application.java

--- oauth2/src/main/java/com/ncs/springcloud/Oauth2Application.java
+++ oauth2/src/main/java/com/ncs/springcloud/Oauth2Application.java
@@ -2,14 +2,13 @@ package com.ncs.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 @RestController
 @EnableRedisHttpSession
