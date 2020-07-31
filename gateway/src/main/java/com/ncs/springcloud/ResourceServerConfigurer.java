68f95063542c04e164cd50fae0389b28c034917e
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-31 11:44:59 +0800

    Update ResourceServerConfigurer.java

--- gateway/src/main/java/com/ncs/springcloud/ResourceServerConfigurer.java
+++ gateway/src/main/java/com/ncs/springcloud/ResourceServerConfigurer.java
@@ -1,9 +1,9 @@
 package com.ncs.springcloud;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+// import org.springframework.security.config.web.server.ServerHttpSecurity;
+// import org.springframework.security.web.server.SecurityWebFilterChain;
 
 //@EnableWebFluxSecurity
 public class ResourceServerConfigurer {
