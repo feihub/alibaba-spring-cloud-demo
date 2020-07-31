bde409ac5ac37e0b5f9e5b9bdc9d6559331ed4fc
wangxiaofei <XiaofeiWang2016@163.com>
2020-07-31 10:49:12 +0800

    Update Business2Application.java

--- business2/src/main/java/com/ncs/springcloud/Business2Application.java
+++ business2/src/main/java/com/ncs/springcloud/Business2Application.java
@@ -5,13 +5,12 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 @RestController
 @EnableRedisHttpSession
